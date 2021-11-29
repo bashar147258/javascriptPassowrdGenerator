bd9452c6942c854e02386b11b5ba58d278d769f9
bashar147258 <89011274+bashar147258@users.noreply.github.com>
2021-11-29 23:07:32 +1100

    Removed Unused code

--- script.js
+++ script.js
@@ -3,29 +3,7 @@ var currentTheme = localStorage.getItem('theme');
 var generateBtn = document.getElementById("generate");
 const passLenAmount = document.getElementById("passLen");
 const passLenSlider = document.getElementById("passLenNum");
-/*
-if (currentTheme) {
-    document.documentElement.setAttribute('data-theme', currentTheme);
-  
-    if (currentTheme === 'dark') {
-        themeSwitch.checked = true;
-    }
-}
 
-function switchTheme(event) {
-    if (event.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark');
-    }
-    else { 
-        document.documentElement.setAttribute('data-theme', 'light');
-        localStorage.setItem('theme', 'light');
-    }    
-}
-
-themeSwitch.addEventListener('change', switchTheme, false);
-*/
-//Code for CSS  theme switch retrived from https://dev.to/ananyaneogi/create-a-dark-light-mode-switch-with-css-variables-34l8
 
 function writePassword() {
     let password =''
