5f8cc1095ee763d78e3b88025de7bf38c8d5bb77
bashar147258 <89011274+bashar147258@users.noreply.github.com>
2021-11-29 22:55:12 +1100

    Fixed Script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-
-var toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+var themeSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 var currentTheme = localStorage.getItem('theme'); 
-var generateBtn = document.querySelector("#generate");
-
-
+var generateBtn = document.getElementById("generate");
+const passLenAmount = document.getElementById("passLen");
+const passLenSlider = document.getElementById("passLenNum");
+/*
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
   
     if (currentTheme === 'dark') {
-        toggleSwitch.checked = true;
+        themeSwitch.checked = true;
     }
 }
 
@@ -23,6 +23,69 @@ function switchTheme(event) {
     }    
 }
 
-toggleSwitch.addEventListener('change', switchTheme, false);
-
+themeSwitch.addEventListener('change', switchTheme, false);
+*/
 //Code for CSS  theme switch retrived from https://dev.to/ananyaneogi/create-a-dark-light-mode-switch-with-css-variables-34l8
+
+function writePassword() {
+    let password =''
+    let length = passLenAmount.value
+
+    let lowerCase= document.getElementById("lowercase").checked;
+    let upperCase=document.getElementById("uppercase").checked;
+    let number =document.getElementById("numbers").checked;
+    let symbol=document.getElementById("special").checked;
+
+    //Boolean true also equals 1. If none of the options are selected, the result will be 0
+    if (lowerCase + upperCase + number + symbol === 0) {
+        return document.getElementById("password").textContent ="INVALID SELECTION";
+    }
+    
+    //Iterate over the length
+    for (i=0;i < length; i++) {
+        //Will randomly generate a value between 1 and 3
+        const r = (Math.floor(Math.random() * 3)+1)
+        
+        if (lowerCase && r===0) {
+            password += generateRandomLower();
+        } else if (upperCase && r===1) {
+            password += generateRandomUpper();
+        } else if (number && r===2) {
+            password += generateRandomNumber();
+        } else if (symbol && r===3) {
+            password += generateRandomSymbol();
+        } else {
+            //If none of the conditions are true, it will decrement i essentially reseting until the above condition is met
+            i--; 
+        }
+    }
+    document.getElementById("password").textContent = password;
+}
+
+passLenAmount.addEventListener('input', syncPassValue);
+passLenSlider.addEventListener('input', syncPassValue);
+generateBtn.addEventListener('click', writePassword);
+
+function syncPassValue(e) {
+    //Syncs the numerical value aswell as the slider
+    const value = e.target.value
+    passLenAmount.value = value;
+    passLenSlider.value = value;
+}
+
+function generateRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random()*26)+97);
+}
+
+function generateRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random()*26)+65);
+}
+
+function generateRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random()*10)+48);
+}
+
+function generateRandomSymbol() {
+    const symbols =  "~*$%@#^&!?*'-=/,.{}()[]<>"
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
